060fa6877efd72e97ca4ee621ece0ef006bc1de9
Muhammad Shoaib <wmfcagency@gmail.com>
2025-11-16 11:52:25 +0500

    updating again/ once more

--- src/components/Pages/CaseStudyDetailsPage.jsx
+++ src/components/Pages/CaseStudyDetailsPage.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { caseStudies } from '../data/caseStudies';
 import { pageTitle } from '../../helper';
 import Cta from '../Cta';
 import Div from '../Div';
@@ -6,9 +8,14 @@ import PageHeading from '../PageHeading';
 import SectionHeading from '../SectionHeading';
 import Spacing from '../Spacing';
 
-export default function CaseStudyDetailsPage({ caseStudy }) {
+export default function CaseStudyDetailsPage() {
+  const { id } = useParams();
+  const caseStudy = caseStudies.find((cs) => cs.id === id);
+
+  if (!caseStudy) return <p>Case study not found.</p>;
+
   pageTitle(caseStudy.title);
-  
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -24,31 +31,50 @@ export default function CaseStudyDetailsPage({ caseStudy }) {
 
       <Div className="container">
         <SectionHeading
-          title={caseStudy.heading}
+          title={caseStudy.title}
           subtitle={caseStudy.category}
           variant="cs-style1 text-center"
         />
         <Spacing lg="90" md="45" />
-        <img src={caseStudy.images[0]} alt="Thumb" className="w-100 cs-radius_15" />
-        <Spacing lg="140" md="80" />
 
+        {caseStudy.images.map((img, i) => (
+          <img key={i} src={img} alt={`Case Study ${i}`} className="w-100 cs-radius_15" />
+        ))}
+
+        <Spacing lg="60" md="45" />
         <h2 className="cs-font_38 text-center">Case Study Overview</h2>
         <Spacing lg="60" md="45" />
         <p className="cs-m0">{caseStudy.overview}</p>
 
-        <Spacing lg="65" md="45" />
-        <Div className="row">
-          {caseStudy.images.slice(1, 3).map((img, index) => (
-            <Div key={index} className="col-sm-6">
-              <img src={img} alt={`Thumb ${index}`} className="w-100 cs-radius_5" />
-              <Spacing lg="25" md="25" />
+        {caseStudy.research.map((res, idx) => (
+          <Div className="row align-items-center" key={idx}>
+            <Div className="col-lg-5">
+              <h3 className="cs-font_30 cs-m0">{res.title}</h3>
+              <Spacing lg="45" md="30" />
+              <p className="cs-m0">{res.description}</p>
             </Div>
-          ))}
-        </Div>
-        <Spacing lg="125" md="55" />
+            <Div className="col-lg-6 offset-lg-1 text-center">
+              <img src={res.image} alt={res.title} className="w-100" />
+              <Spacing lg="0" md="40" />
+            </Div>
+          </Div>
+        ))}
+
+        <Spacing lg="100" md="80" />
+        <h2 className="cs-font_38 text-center">Result of The Case Study</h2>
+        <Spacing lg="60" md="45" />
+        <p className="cs-m0">{caseStudy.result}</p>
       </Div>
 
-      {/* Repeat similar structure for Research, Results, and CTA sections */}
+      <Spacing lg="145" md="80" />
+      <Div className="container">
+        <Cta
+          title="Let’s discuss and make <br />something <i>cool</i> together"
+          btnText="Apply For Meeting"
+          btnLink="/contact"
+          bgSrc="/images/cta_bg.jpeg"
+        />
+      </Div>
     </>
   );
 }
