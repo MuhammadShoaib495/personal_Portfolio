7c05a933438e6e21c79a91cb13736e13b99f0f20
Muhammad Shoaib <wmfcagency@gmail.com>
2025-11-16 11:46:35 +0500

    uploaded data folder

---
+++ src/data/caseStudies.js
@@ -0,0 +1,54 @@
+export const caseStudies = [
+  {
+    id: '47-consultants',
+    title: '47 Consultants Video Marketing Agency',
+    heroImage: '/images/slider_5.jpeg',
+    category: 'Marketing',
+    overview:
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...',
+    research: [
+      {
+        title: 'Marketing strategy',
+        description:
+          'Our goal perspiciatis unde omnis iste natus error sit voluptatem bolaru accusantium...',
+        image: '/images/case_study_img_4.png',
+      },
+      {
+        title: 'Branding guideline',
+        description:
+          'Our goal perspiciatis unde omnis iste natus error sit voluptatem bolaru accusantium...',
+        image: '/images/case_study_img_5.png',
+      },
+    ],
+    images: [
+      '/images/case_study_img_1.jpeg',
+      '/images/case_study_img_2.jpeg',
+      '/images/case_study_img_3.jpeg',
+    ],
+    result:
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...',
+    link: 'https://47consultants.com',
+  },
+  {
+    id: 'real-estate',
+    title: 'Real Estate Case Study',
+    heroImage: '/images/slider_8.jpeg',
+    category: 'Real Estate',
+    overview: 'Overview text for Real Estate case study...',
+    research: [],
+    images: ['/images/slider_8.jpeg'],
+    result: 'Results text for Real Estate case study...',
+    link: 'https://realestate.wmfcagency.com',
+  },
+  {
+    id: 'william-barber',
+    title: 'William Barber LLC',
+    heroImage: '/images/slider_6.jpeg',
+    category: 'Business',
+    overview: 'Overview text for William Barber case study...',
+    research: [],
+    images: ['/images/slider_6.jpeg'],
+    result: 'Results text for William Barber case study...',
+    link: '/case-study/william-barber',
+  },
+];
