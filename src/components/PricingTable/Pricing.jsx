4615010263e8cf2a568a930e29f307fc4f3c91dd
Muhammad Shoaib <wmfcagency@gmail.com>
2026-08-29 17:07:58 +0500

    Updated Once Again Pricing Jsx

--- src/components/PricingTable/Pricing.jsx
+++ src/components/PricingTable/Pricing.jsx
@@ -5,13 +5,14 @@ import Spacing from "../Spacing";
 function Pricing() {
   return (
     <>
-      {pricingData.map((pricing, index) => (
-        <PricingTable
-          key={index}
-          {...pricing}
-        />
-      ))}
-
+      {features.map((feature, index) => (
+  <li key={index}>
+    <strong>{feature.serviceName}</strong>
+    <p>{feature.description}</p>
+    <span>{feature.hrs}</span>
+    <span>{feature.price}</span>
+  </li>
+))}
       <Spacing lg={20} md={20} />
 
       <PricingTable
