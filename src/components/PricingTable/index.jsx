1bcd39e828933024d407f39245825d761f21a28a
Muhammad Shoaib <wmfcagency@gmail.com>
2026-08-29 17:38:12 +0500

    Pricing Updated index.js

--- src/components/PricingTable/index.jsx
+++ src/components/PricingTable/index.jsx
@@ -1,33 +1,95 @@
-import React from 'react'
-import { Icon } from '@iconify/react';
-import './pricing.scss'
-import Button from '../Button';
-import Div from '../Div';
+import React from "react";
+import "./pricing.scss";
+import Div from "../Div";
+import Spacing from "../Spacing";
 
-export default function PricingTable({title, price, currency, features, btnLink, btnText, timeline}) {
-  return (
-    <Div className="cs-pricing_table cs-style1">
-      <h2 className="cs-pricing_title">{title}</h2>
-      <Div className="cs-pricing_info">
-        <Div className="cs-price">
-          <h3 className="cs-white_color">{currency}{price}/</h3>
-          <span className="cs-accent_color">{timeline}</span>
-        </Div>
-        <Div className="cs-price_text">Per subscriber per year</Div>
+export default function PricingTable({
+title,
+totalPrice,
+price,
+currency,
+features = [],
+timeline,
+}) {
+const featureList = Array.isArray(features) ? features : [];
+
+return ( <Div className="container"> <Div className="cs-pricing_table cs-style1">
+
+    <h2 className="cs-pricing_title">
+      {title}
+    </h2>
+
+    <Div className="cs-pricing_info">
+      <Div className="cs-price">
+        <h3 className="cs-white_color">
+          {currency || ""}
+          {price || ""}
+        </h3>
+
+        <span className="cs-accent_color">
+          {timeline || ""}
+        </span>
       </Div>
-      <ul className="cs-pricing_feature cs-mp0">
-        {features.map((feature, index)=>(
-          <li key={index}>
-            <span className="cs-feature_icon cs-accent_color">
-              <Icon icon="bi:arrow-right-circle" />
-            </span>
-            <span>{feature}</span>
-          </li>
-        ))}
-      </ul>
-      <Div className="cs-pricing_btn_wrap">
-        <Button btnLink={btnLink} btnText={btnText}/>
+
+      <Div className="cs-price_text">
+        {totalPrice || ""}
       </Div>
     </Div>
-  )
+
+    <ul className="cs-pricing_feature cs-mp0">
+      <li className="responsive-table">
+
+        <table>
+          <colgroup>
+            <col style={{ width: "20%" }} />
+            <col style={{ width: "50%" }} />
+            <col style={{ width: "15%" }} />
+            <col style={{ width: "15%" }} />
+          </colgroup>
+
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Description</th>
+              <th>Hours</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {featureList.map((feature, index) => (
+              <tr key={index}>
+                <td>
+                  {feature?.serviceName || ""}
+                </td>
+
+                <td>
+                  {feature?.description || ""}
+                </td>
+
+                <td>
+                  {feature?.hrs || ""}
+                </td>
+
+                <td>
+                  {feature?.price || ""}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <Spacing lg={20} md={20} />
+
+      </li>
+    </ul>
+
+    <Div className="cs-pricing_btn_wrap" />
+
+  </Div>
+
+  <Spacing lg={20} md={20} />
+</Div>
+
+);
 }
