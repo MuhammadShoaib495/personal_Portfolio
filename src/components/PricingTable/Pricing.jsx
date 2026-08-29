151c7eb9b8e248f0d03b2c1c67fa9181ae17289a
Muhammad Shoaib <wmfcagency@gmail.com>
2026-08-29 15:50:59 +0500

    uploaded Pricing and Service Page

---
+++ src/components/PricingTable/Pricing.jsx
@@ -0,0 +1,29 @@
+import { pricingData, additionalServices } from "../../data/pricingData";
+import PricingTable from "../PricingTable/index";
+import Spacing from "../Spacing";
+
+function Pricing() {
+  return (
+    <>
+      {pricingData.map((pricing, index) => (
+        <PricingTable
+          key={index}
+          {...pricing}
+        />
+      ))}
+      <Spacing lg={20} md={20}/>
+        <PricingTable
+        totalPrice="Additional Services"
+        title={additionalServices.title}
+        price={null}
+        timeline={null}
+        currency={null}
+        btnLink={null}
+        btnText={null}
+        features={additionalServices.features}
+      />
+    </>
+  );
+}
+
+export default Pricing;
---
+++ src/components/PricingTable/ServicePage.jsx
@@ -0,0 +1,38 @@
+import Pricing from '../PricingTable/Pricing';
+import Div from '../Div';
+import Spacing from '../Spacing';
+import { pageTitle } from '../../helper';
+import Cta from '../Cta';
+
+
+
+export default function ServicePage() {
+      pageTitle('SEO Service Pricing');
+    
+    return(
+        
+    <Div className="container">  
+          <Spacing lg="145" md="80" />
+      
+         <Div className="cs-section_heading">
+              <h3 className="cs-section_subtitle text-center">SEO</h3>
+              <h2 className="cs-section_title text-center">Complete Pricing List</h2>
+                 <Spacing lg="40" md="30"/>            
+      <div className="cs-shape_1" />
+     {/* Pricing Table */}
+      <Pricing/>
+       {/* End Pricing Table */} 
+        {/* Start CTA Section */}
+             <Cta
+               title="Let’s disscuse make <br />something <i>cool</i> together"
+               btnText="Let's Discuss"
+               btnLink="mailto:wmfcagency@gmail.com"
+               bgSrc="/images/cta_bg_5.jpeg"
+               variant="cs-type_1"
+             />
+             {/* End CTA Section */}
+       </Div></Div>
+       
+    );
+   
+}
